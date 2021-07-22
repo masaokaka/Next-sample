1b37ecdd55df11e972e902437468308160258258
masaokaka <osushiiisensu@gmail.com>
2021-07-22 15:56:12 +0900

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
 import "../styles/globals.css";
-import { SampleContextProvider } from "../store/sample-context";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <SampleContextProvider>
-      <Component {...pageProps} />
-    </SampleContextProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
